379f4d0215d4068761ccb1c2359f50cd4c4d436b
mnosumon <mdnuruddin378350@gmail.com>
2025-06-30 16:09:00 +0600

    first commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { View, Text, Button } from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>
+        react native android emulator when run this project then emulator status
+        bar hide
+      </Text>
+      <Button title="presss me" />
+    </View>
+  );
+};
+
+export default App;
